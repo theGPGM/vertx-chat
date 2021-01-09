39162a01f39ce9207252eb3db4c18d9ef68f56e7
theGPGM <570388779@qq.com>
2021-01-09 18:10:15 +0800

    修改

--- src/main/java/org/george/dungeon_game/cache/impl/PlayerLevelCacheImpl.java
+++ src/main/java/org/george/dungeon_game/cache/impl/PlayerLevelCacheImpl.java
@@ -1,9 +1,15 @@
 package org.george.dungeon_game.cache.impl;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.george.dungeon_game.cache.PlayerLevelCache;
 import org.george.dungeon_game.cache.bean.PlayerLevelCacheBean;
+import org.george.dungeon_game.dao.PlayerLevelDao;
+import org.george.dungeon_game.dao.bean.PlayerLevelBean;
 import org.george.util.ThreadLocalJedisUtils;
 import redis.clients.jedis.Jedis;
+
+import java.io.IOException;
 
 public class PlayerLevelCacheImpl implements PlayerLevelCache {
 
@@ -15,17 +21,34 @@ public class PlayerLevelCacheImpl implements PlayerLevelCache {
         return instance;
     }
 
+    private PlayerLevelDao playerLevelDao = PlayerLevelDao.getInstance();
+
     @Override
     public PlayerLevelCacheBean getPlayerLevelByPlayerId(Integer playerId) {
         Jedis jedis = ThreadLocalJedisUtils.getJedis();
+        ObjectMapper objectMapper = new ObjectMapper();
         PlayerLevelCacheBean level = null;
-        if(jedis.exists("player_level#" + playerId).booleanValue()){
+        if(jedis.exists("player_level#" + playerId)){
+            String json = jedis.get("player_level#" + playerId);
+            try {
+                level = objectMapper.readValue(json, PlayerLevelCacheBean.class);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }else{
+            PlayerLevelBean bean = playerLevelDao.loadPlayerLevelByPlayerId(playerId);
+
             level = new PlayerLevelCacheBean();
-            level.setPlayerId(playerId);
-            Integer playerLevel = Integer.parseInt(jedis.hget("player_level#" + playerId, "level"));
-            Integer loseCount = Integer.parseInt(jedis.hget("player_level#" + playerId, "lose_count"));
-            level.setLevel(playerLevel);
-            level.setLoseCount(loseCount);
+            level.setPlayerId(bean.getPlayerId());
+            level.setLevel(bean.getLevel());
+            level.setLoseCount(bean.getLoseCount());
+
+            try {
+                String json = objectMapper.writeValueAsString(level);
+                jedis.set("player_level#" + level.getPlayerId(), json);
+            } catch (JsonProcessingException e) {
+                e.printStackTrace();
+            }
         }
         return level;
     }
@@ -33,18 +56,39 @@ public class PlayerLevelCacheImpl implements PlayerLevelCache {
     @Override
     public void updatePlayerLevelSelective(PlayerLevelCacheBean level) {
         Jedis jedis = ThreadLocalJedisUtils.getJedis();
+        ObjectMapper objectMapper = new ObjectMapper();
+        String json = jedis.get("player_level#" + level.getPlayerId());
+        try {
+            PlayerLevelCacheBean oldCacheBean = objectMapper.readValue(json, PlayerLevelCacheBean.class );
+            if(level.getLevel() == null){
+                level.setLevel(oldCacheBean.getLevel());
+            }
+            if(level.getLoseCount() == null){
+                level.setLevel(oldCacheBean.getLoseCount());
+            }
+            String newJson = objectMapper.writeValueAsString(level);
+            jedis.set("player_level#" + level.getPlayerId(), newJson);
 
-        if(level.getLevel() != null)
-            jedis.hset("player_level#" + level.getPlayerId(), "level", String.valueOf(level.getLevel()));
-
-        if(level.getLevel() != null)
-            jedis.hset("player_level#" + level.getPlayerId(), "lose_count", String.valueOf(level.getLoseCount()));
+            PlayerLevelBean bean = new PlayerLevelBean();
+            bean.setLevel(level.getLevel());
+            bean.setLoseCount(level.getLoseCount());
+            bean.setPlayerId(level.getPlayerId());
+            playerLevelDao.updateRecordSelective(bean);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void addPlayerLevel(PlayerLevelCacheBean level) {
         Jedis jedis = ThreadLocalJedisUtils.getJedis();
-        jedis.hset("player_level#" + level.getPlayerId(), "level", String.valueOf(level.getLevel()));
-        jedis.hset("player_level#" + level.getPlayerId(), "lose_count", String.valueOf(level.getLoseCount()));
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            String json = objectMapper.writeValueAsString(level);
+            jedis.set("player_level#" + level.getPlayerId(), json);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        playerLevelDao.addPlayerLevel(level.getPlayerId());
     }
 }
